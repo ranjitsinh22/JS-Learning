5436bb7e5802869b4f2c20b5813015435bc2170b
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-16 11:54:12 +0530

    01_variable _assignment
    JavaScript Variable assignment 1 Completed

---
+++ 01_variable_assignment.js
@@ -0,0 +1,40 @@
+console.log("*******************Step 1 **********************");
+console.log("***Before Initialization***");
+var myFullName="Ranjitsinh Shivaji Gavali";
+console.log("My Full Name:",myFullName);
+
+console.log("***After initialization***");
+MyFullName=" Ranjitsinh Gavali";
+console.log("My Full Name with first and last name only: Ranjitsinh Gavali");
+
+console.log("*****Updated Full Name*****");
+var myFullName="Ranjitsinh Shivaji Gavali"
+console.log(myFullName);
+
+console.log("*******************Step 2******************");
+var myLove="I Love Only JS";
+console.log(myLove);
+
+console.log("*******************Step 3*******************");
+console.log("****Declare and Initialize is On Same Line****");
+var firstName="Ranjitsinh";
+console.log("My First Name :",firstName );
+
+var lastName="Gavali";
+console.log("My Last Name :",lastName );
+
+var myAge="24";
+console.log("My Age :  ",myAge );
+
+var myadress="AP-Akluj TAl-Malshiras Dist-Solapur";
+console.log("My Adress is  :",myadress );
+
+var myPincode="413101";
+console.log("My PinCode is  :",myPincode );
+
+var myGender="Male";
+console.log("My Gender is  :",myGender );
+
+var myMarritalStatus="Unmarried";
+console.log("Marrital Status  :",myMarritalStatus );
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,32 @@
 console.log("Mohit Sharma");
+var fullName="Ranjitsinh Gavali";
+console.log(fullName);
+console.log(fullName);
+console.log(fullName);
+console.log("pune");
+console.log("24");
+
+var city; // this is variable declaration
+console.log(city);
+ 
+city="Pune"; // this is variable initialization
+console.log(city);
+
+city="Solapur";  // variable update
+console.log(city);
+
+//javascript programme execute by node js - v8 engine
+/*javascript is interpreted programming  lagnuage- programme run in line by line.
+varibale chya madhe value assign keli naslyas tyla variable declaration mantat */
+//kontyahi variablee la fiest time vale assign kartat - variable initialization
+
+var age="24";
+var age ="25";
+var age ="26";
+console.log(age);
+
+// var university;  variable declaration
+// university ="SPPU";  variable initialization
+
+var university="SPPU";
+console.log(university);
