18e4b61a1d2db24d8ecf7976934b4cc0440cd1d0
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-17 15:08:58 +0530

    02_typeOfAssignment
    variables,data type and typeof operators

---
+++ 02_typeOfAssignment.js
@@ -0,0 +1,39 @@
+console.log("*******************Step 1 **********************");
+console.log("***Before Initialization***");
+var bankName;            //this is Declaration
+console.log(bankName);
+
+console.log("***After initialization***");
+var bankName="SBI bank";
+console.log("My Bank Name:SBI");
+console.log("typeof Bank Name is:" ,typeof bankName);
+
+
+
+console.log("*******************Step 2 **********************");
+console.log("****Declare and Initialize is On Same Line****");
+var graduationMark=90;
+console.log("My Marks is :",graduationMark);
+console.log("typeof My Marks is :" ,typeof graduationMark);
+
+console.log("*******************Step 3 **********************");
+console.log("***Before Initialization***");
+var isWorking;
+console.log(isWorking);
+
+console.log("***After initialization***");
+isWorking=true;
+console.log("Work Status:true");
+console.log("typeof  Status is:" ,typeof isWorking);
+
+console.log("*******************Step 4 **********************");
+console.log("****Declare and Initialize is On Same Line****");
+var totalCount="Hundred and Seven";
+console.log("Total Count: Hundred and Seven");
+console.log("typeof Total Count is :" ,typeof totalCount);
+
+console.log("****Updated totalCount Variable****");
+var totalCount=107;
+console.log("Total Count: 107");
+console.log("typeof Total Count is :" ,typeof totalCount);
+
