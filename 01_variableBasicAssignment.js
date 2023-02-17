68e5da6b4b9a8cd3b6baffdca45c27deb6f9019c
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-17 15:07:05 +0530

    01_variableBasicAssignment
    variables and null vs undefined

---
+++ 01_variableBasicAssignment.js
@@ -0,0 +1,36 @@
+console.log("******Assignment first*****");
+console.log("****Variable for Each Data Type*****")
+
+console.log("*******Data Type is String****");
+var myName ="Ranjitsinh Gavali"
+var typeOfMyName  = typeof myName;  
+console.log("My Name is:",myName);
+console.log("typeof variable My Name: ", typeOfMyName); 
+
+console.log("********Data Type is Number********");
+var myAge=24;
+var typeOfMyAge  = typeof myAge;  
+console.log("My Age is:",myAge);
+console.log("typeof variable My Age: ", typeOfMyAge);
+
+console.log("********Data Type is Boolean******");
+var isMarried= false;
+var typeOfisMarried  = typeof isMarried;  
+console.log("isMarried:",isMarried);
+console.log("typeof variable My Marrital Status: ", typeOfisMarried);
+
+console.log("*************************************************************************");
+console.log("*****Difference between null and undefined*****");
+
+console.log("*****Data Type is undefined and undefined variable******");
+
+var myAdress;
+var typeOfmyAdress  = typeof myAdress;  
+console.log("My Adress:",myAdress);
+console.log("typeof variable My Adress: ", typeOfmyAdress);
+
+console.log("*****Data Type is Object and null variable******");
+var myAdress=null;
+var typeOfmyAdress  = typeof myAdress;  
+console.log("My Adress:",myAdress);
+console.log("typeof variable My Adress: ", typeOfmyAdress);
