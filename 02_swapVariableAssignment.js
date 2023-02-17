3f15af14c30bc969ec7769b25cecdaba3fe932cb
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-17 15:10:25 +0530

    02_swapVariablesAssignment
    Swapping two and three variables

---
+++ 02_swapVariableAssignment.js
@@ -0,0 +1,26 @@
+console.log("**********Swapping Variables ************");
+console.log("****Before Swapping Variables*****");
+var sweety="Sweety";      //put value of num2 in num1
+var cutie="Cutie";
+console.log(sweety,cutie);
+
+console.log("*****Afrer Swapping Variables******");
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log(sweety,cutie);
+
+console.log("**********Step2 ************");
+console.log("Swapping Variables");
+console.log("****Before Swapping Variables*****");
+var num1=100;      //put value of num2 in num1
+var num2=200;
+var num3=300
+console.log(num1,num2,num3);
+
+console.log("*****Afrer Swapping Variables******");
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log(num1,num2,num3);
