47645e9eebd92883a8416f3a758ff3b483b70478
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-20 14:17:07 +0530

    03_functionAssignment01
    03_functionAssignment01

---
+++ 03_functionAssignment01.js
@@ -0,0 +1,48 @@
+console.log("*******step1*******");
+//function with no argument
+function myFavourite()
+{
+    console.log("function with no argument");
+    console.log("My favourite Car: Mustang GT");
+}
+myFavourite("My favourite Car: Mustang GT");
+
+console.log("********Step 2*********");
+var firstName="Ranjitsinh"
+var lastName="Gavali"
+var collegeName="AGC Pune"
+function personalDetails(firstName,lastName,collegeName)
+{
+console.log("First Name:Ranjitsinh,Last Name:Gavali,College Name:AGC Pune");
+}
+personalDetails();
+
+console.log("********Step 3*********");
+var name1="Virat";
+var name2="Anushka";
+
+var num1="1000";
+var num2="2000";
+
+function swapValuesDude(value1,value2)   //this is equal to var value1=num1 , var value2=num2       //in variable name total number of argument should not more than 7
+{console.log("before swap:",value1,value2);
+var temp = value1;
+value1=value2;                                      //total number of lines in funtion should not more than 25
+value2=temp;
+console.log("After swap:",value1,value2);
+
+}
+swapValuesDude(name1,name2);
+swapValuesDude(num1,num2);
+
+console.log("*******step 4*********");
+
+function addThreeValues(value1,value2,value3)
+{
+    var sum=value1+value2+value3;
+    return sum;
+}
+var result=addThreeValues(10.23,600,40);
+console.log(result);
+var result=addThreeValues("Hello","Good","Morning")
+console.log(result);
