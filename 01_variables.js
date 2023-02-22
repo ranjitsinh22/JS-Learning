f8041132720015087592d75db6d926ac2df8df75
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-22 12:55:52 +0530

    Create 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,31 @@
+console.log("Personal Datails");
+var fullName="Ranjitsinh Gavali";
+console.log(fullName);
+
+console.log("pune");
+console.log("24");
+
+var city; // this is variable declaration
+console.log(city);
+ 
+city="Pune"; // this is variable initialization
+console.log(city);
+
+city="Solapur";  // variable update
+console.log(city);
+
+//javascript programme execute by node js - v8 engine
+/*javascript is interpreted programming  lagnuage- programme run in line by line.
+varibale chya madhe value assign keli naslyas tyla variable declaration mantat */
+//kontyahi variable la first time value assign kartat - variable initialization
+
+var age="24";
+var age ="25";
+var age ="26";
+console.log(age);
+
+// var university;  variable declaration
+// university ="SPPU";  variable initialization
+
+var university="SPPU";  // Declare and Initialize is On Same Line
+console.log(university);
