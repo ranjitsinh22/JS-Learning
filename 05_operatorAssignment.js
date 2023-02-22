c26902d15ebe0d279e6cb74de70e228061869605
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-02-22 12:53:18 +0530

    05_operatorAssignment

---
+++ 05_operatorAssignment.js
@@ -0,0 +1,46 @@
+console.log(`=====step1=======`);
+console.log(`1)`);
+var javaScript="JavaScript";
+console.log(`The Lenght of JavaScript:${javaScript.length}`);
+
+var javaScriptLength=10
+var result=javaScriptLength*javaScriptLength;
+console.log(`Sqaure of JavaScript Length is:${result}`);
+
+console.log(`2)`);
+var googleChrome="Google Chrome";
+console.log(`The Lenght of Google Chrome:${googleChrome.length}`);
+
+var googleChromeLength=13
+var result=googleChromeLength*googleChromeLength;
+console.log(`Sqaure ofGoogle ChromeLength is:${result}`);
+
+console.log(`3)`);
+var developerSmart="Developer Smart";
+console.log(`The Lenght of Developer Smart:${developerSmart.length}`);
+
+var developerSmartLength=15
+var result=developerSmartLength*developerSmartLength;
+console.log(`Sqaure of Developer Smart Length is:${result}`);
+
+console.log(`========step2==========`);
+console.log(`1)`);
+function profession()
+{
+    console.log("Given String is:I am Angular Developer");
+}
+profession();
+
+console.log(`2)`);
+var profession="I am Angular Developer"
+console.log(`The Lenght of String:${profession.length}`);
+
+var profession= profession.split(" ");
+console.log("Total words: ", profession.length);
+
+console.log(`3)`);
+result = 22/ 4;
+console.log(` String Lenght divided by total number of word: ${result} `);
+
+var result=22*4;
+console.log(` String Lenght Multiply by total number of word: ${result} `);
