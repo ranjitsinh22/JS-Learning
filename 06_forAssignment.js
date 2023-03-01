7d997e7d1e93761a066a50da74f0bb5f38c812eb
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-03-01 11:56:39 +0530

    for loop assignment

---
+++ 06_forAssignment.js
@@ -0,0 +1,34 @@
+console.log(`-----Q.1)Print number from 5 to 15 by increment 1------`);
+for (let index = 5; index<=15; index=index +1) {
+    console.log(`The number from 5 to 15 are : ${index}`);
+}
+
+console.log(`-----Q.2)Print number from 50 to 40 by decrement by 1------`);
+for (let index = 50; index>=40; index=index-1) {
+    console.log(`The number from 50 to 40 are : ${index}`);  
+}
+
+console.log(`-----Q.3)Find first 15 ODD number------`);
+for (let index = 1; index<=30; index=index+2) {
+    console.log(`The first 15 ODD numbers are : ${index}`); 
+}
+    
+console.log(`-----Q.4)Find first 10 EVEN number------`);
+for (let index = 0; index<=20; index=index+2) {
+    console.log(`The first 10 EVEN numbers are : ${index}`); 
+}
+
+console.log(`-----Q.5)Print table of 5 in increasing order------`);
+for (let index = 5; index <= 50; index = index + 5) {
+    console.log(`The talbe of 5 is : ${index}`);
+}   
+
+console.log(`-----Q.6)Print table of 10 in increasing order-----`);
+for (let index= 10; index <= 100; index = index + 10) {
+    console.log(`The talbe of 10 is : ${index}`);
+} 
+
+console.log(`-----Q.7)Print table of 10 in decreasing order-----`);
+for (let index = 100; index>=10; index = index - 10) {
+    console.log(`The talbe of 10 in reverse order is : ${index}`);
+}
