2c266bb21764cc0c2d92683f1567c291f07d9ae5
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-03-03 15:04:33 +0530

    For String Assignment 02/03/2023

---
+++ 04_stringTemplate.js
@@ -0,0 +1,13 @@
+console.log("You are champian");
+console.log(`you are "champian"`);
+
+var firstName="Mohit";
+var lastName="Sharma";
+var city="Pune";
+
+console.log("My first name is",firstName ,"My last name is",lastName,"My city is",city);
+
+// console.log(My first name is ${firstName} My last name is ${lastName} My city is ${city}`);
+
+
+
